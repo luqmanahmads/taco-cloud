6bb0c4b00b8c30b94c11e9cae441cb984e26d173
Luqman Ahmad <luqman_ahmads@ymail.com>
2021-11-30 15:21:26 +0700

    tambah home controller

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,13 +1,22 @@
 package tacos;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import tacos.controller.HomeController;
 
 @SpringBootTest
 class TacoCloudApplicationTests {
 
+	@Autowired
+	private HomeController controller;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
